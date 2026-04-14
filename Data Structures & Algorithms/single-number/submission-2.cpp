c4671ed7b53a74291f56ceac1d218e65b6696687
M.M.Nayem <mmnayem25@gmail.com>
2026-04-14 15:46:24 +0600

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+
+        int i=0;
+
+        while(i<nums.size())
+        {
+            if(nums[i]==nums[i+1])
+            {
+                i+=2;
+            }
+            else
+              return nums[i];
+        }
+        
+    }
+};
